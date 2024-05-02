140096bdbb5959ab4c07aeb68fc0fb4af9e3e73f
김다온 <128460925+kdaon07@users.noreply.github.com>
2024-05-02 11:53:47 +0900

    2024-05-02

---
+++ Day08/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import MsgList2 from './MsgList2'
+import MessageList3 from './MessageList3'
+
+function App() {
+  
+  return (
+    // <MsgList2 />
+    <MessageList3 />
+  );
+}
+
+export default App
---
+++ Day08/src/MsgList2.jsx
@@ -0,0 +1,74 @@
+import { useState, useRef } from "react";
+import "./style.css";
+
+const msgs = [
+    "안녕하세요. 오늘의 일정입니다.",
+    "점심시간이 11시 30분으로 변경되었습니다.",
+    "이제 곧 회의가 시작됩니다."
+];
+
+export default function MsgListLast() {
+    const [input, setInput] = useState('');
+    const [msgList, setMsgList] = useState([
+        {
+            id: 1,
+            msg: msgs[0],
+        },
+        {
+            id: 2,
+            msg: msgs[1],
+        },
+        {
+            id: 3,
+            msg: msgs[2],
+        }
+    ]);
+
+    const nextId = useRef(4);
+
+    const onCreate = () => {
+        const newMsg = {
+            id: nextId.current,
+            msg: input,
+        };
+        setMsgList([...msgList, newMsg]);
+        nextId.current += 1;
+    }
+
+    const onRemove = (id) => {
+        const filteredList = msgList.filter((msg1) => msg1.id !== id);
+        setMsgList(filteredList);
+    }
+
+    const onModify = (id) => {
+        setMsgList(
+            msgList.map(msg1 => msg1.id === id ? {
+                ...msg1,
+                msg: input,
+            } : msg1)
+        );
+    }
+
+    return (
+        <div>
+            <h1>일정 관리</h1>
+            <div className="container">
+                <input type="text" name="msg" value={input} onChange={(e) => setInput(e.target.value)} />
+                <button onClick={onCreate}>등록</button>
+                <button onClick={() => setInput('')}>취소</button>
+            </div>
+            {
+                msgList.map(({id, msg}) => (
+                    <div>
+                        <div className="wrapper" onClick={() => setInput(msg)}>{msg}
+                            <div className="control">
+                                <button onClick={() => onRemove(id)}>삭제</button>
+                                <button onClick={() => onModify(id)}>수정</button>
+                            </div>
+                        </div>
+                    </div>
+                ))                
+            }
+        </div>
+    );
+}
